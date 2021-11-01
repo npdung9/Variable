9caeaccda4bc9e57b76fb84fa71ff097131c3dff
DUNG <dungnguyen9903@gmail.com>
2021-11-01 09:01:20 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,24 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		/**
+		 * Khai báo biến
+		 */
+		String firstName= "Nguyen Phi";
+		String lastName= "Dung";
+		int old= 18;
+		float markMath= 9f;
+		boolean passed= true;
+		
+		System.out.println("Ho va ten:");
+		System.out.println(firstName+ " "+ lastName);
+		System.out.println("Mark Math: "+ markMath);
+		System.out.println("Passed: "+ passed); 
+		
+		//Khai báo hằng. Hằng phải viết hoa
+		final float PI= 3.14f;
+	}
+}
+		
